dff1fc9173709b9d5902e0cb49cedb3afce79b30
assilbekov <askhat.assilbekov@gmail.com>
2024-06-17 14:34:32 -0300

    init transport interfaces

---
+++ p2p/transport.go
@@ -0,0 +1,11 @@
+package p2p
+
+// Peer is an interface that represents a node in the network.
+type Peer interface {
+	//
+}
+
+// Transport is an interface that handles the communication between the nodes in the network.
+type Transport interface {
+	//
+}
