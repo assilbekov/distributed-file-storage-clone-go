9d57ee35f83050576911f36957fb9f818ff615a8
assilbekov <askhat.assilbekov@gmail.com>
2024-06-30 12:33:20 -0300

    update interfaces

--- p2p/transport.go
+++ p2p/transport.go
@@ -2,11 +2,12 @@ package p2p
 
 // Peer is an interface that represents a node in the network.
 type Peer interface {
-	//
+	Close() error
 }
 
 // Transport is an interface that handles the communication between the nodes in the network.
 // This can be a (TCP, UDP, websockets, etc.) connection.
 type Transport interface {
 	ListenAndAccept() error
+	Consume() <-chan RPC
 }
