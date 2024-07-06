6a6d7b299a35201fae32e5e934547648d70d19d2
assilbekov <askhat.assilbekov@gmail.com>
2024-07-06 16:45:04 -0300

    update test

--- store_test.go
+++ store_test.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 )
 
 func TestPathTransformFunc(t *testing.T) {
 	key := "catsbestpictures"
 	pathname := CASPathTransformFunc(key)
-	fmt.Printf("pathname: %s\n", pathname)
+	expectedPathname := "e24fc/4bc21/80e4d/f3696/836ab/8ccb8/ebe1b/7bf9b"
+	if pathname != expectedPathname {
+		t.Fatalf("expected pathname %s, got %s", expectedPathname, pathname)
+	}
 }
 
 func TestStore(t *testing.T) {
