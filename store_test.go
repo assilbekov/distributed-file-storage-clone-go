92dd64673e7c047228b0b968cec6287a713f9b47
assilbekov <askhat.assilbekov@gmail.com>
2024-07-19 14:49:34 -0300

    pause

--- store_test.go
+++ store_test.go
@@ -10,14 +10,14 @@ import (
 func TestPathTransformFunc(t *testing.T) {
 	key := "catsbestpictures"
 	pathKey := CASPathTransformFunc(key)
-	expectedOriginalKey := "e24fc4bc2180e4df3696836ab8ccb8ebe1b7bf9b"
+	expectedFilename := "e24fc4bc2180e4df3696836ab8ccb8ebe1b7bf9b"
 	expectedPathname := "e24fc/4bc21/80e4d/f3696/836ab/8ccb8/ebe1b/7bf9b"
 	if pathKey.PathName != expectedPathname {
 		t.Fatalf("expected pathname %s, got %s", expectedPathname, pathKey.PathName)
 	}
 
-	if pathKey.Filename != expectedOriginalKey {
-		t.Fatalf("expected original key %s, got %s", expectedOriginalKey, pathKey.Filename)
+	if pathKey.Filename != expectedFilename {
+		t.Fatalf("expected original key %s, got %s", expectedFilename, pathKey.Filename)
 	}
 }
 
