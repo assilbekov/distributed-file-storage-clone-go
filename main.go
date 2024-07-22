7916dc7792aaac5308baca72008516aea18dbcb1
assilbekov <askhat.assilbekov@gmail.com>
2024-07-22 08:37:54 -0300

    refactor main func

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/assilbekov/distributed-file-storage-clone-go/p2p"
+import (
+	"github.com/assilbekov/distributed-file-storage-clone-go/p2p"
+	"log"
+)
 
 func main() {
-	tcpTransportOpts := p2p.TCPTransportOpts{}
-	tcpTransport := p2p.NewTCPTransport()
+	tcpTransportOpts := p2p.TCPTransportOpts{
+		ListedAddr:    ":8080",
+		HandshakeFunc: p2p.NOPHandshakeFunc,
+		Decoder:       p2p.DefaultDecoder{},
+		// TODO: onPeer func.
+	}
+	tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
 	fileServerOpts := FileServerOpts{
-		ListenAddr:        ":8080",
 		StorageRoot:       "8080_network",
 		PathTransformFunc: CASPathTransformFunc,
+		Transport:         tcpTransport,
+	}
+
+	s := NewFileServer(fileServerOpts)
+
+	if err := s.Start(); err != nil {
+		log.Fatalf("failed to start server: %v", err)
 	}
 }
